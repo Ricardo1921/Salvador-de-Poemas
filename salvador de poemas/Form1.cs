05007de986492fbf8c724b7e645d71445af6fc99
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2020-02-20 11:31:27 +0000

    Add files via upload

---
+++ salvador de poemas/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace salvador_de_poemas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        RTFFile Texto = new RTFFile();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = Texto.texto;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ salvador de poemas/RTFFile.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace salvador_de_poemas
+{
+    class RTFFile
+    {
+        private string _path = Application.StartupPath + "\\Resources\\9.txt";
+        private string _texto;
+
+        public string texto
+        {
+            get
+            {
+                _texto = File.ReadAllText(_path);
+                return _texto;
+            }
+            set { _texto = value; }
+        }
+
+        public void apagar()
+        
+    }
+}
